10e62fa54df554a51ee05011a44c7a6bcda3c45e
samir.xasiyev <samirakh@code.edu.az>
2024-08-25 12:07:29 +0400

    update task 3 code

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // A, B ededleri arasinda 3e tam bolunen ededleri tapan alqoritm
+
+        for (int i = 0; i <=100; i++)
+        {
+            if (i%3==0 && i>0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+       
+        // Verilmish ededin 2-nin quvveti olub olmadigini
+        // tapan alqoritm misal: 8 quvvetidir, 6 quvveti deyil
+
+        int data = 8, number = data;
+        bool value = true;
+        for (; data > 0; data /= 2)
+        {
+            if (data % 2 != 0 && data != 1)
+            {
+                Console.WriteLine($"{number} ededi 2 quvveti deyil.");
+                value = false;
+                break;
+            }
+        }
+        if (value)
+        {
+            Console.WriteLine($"{number} ededi 2 quvvetidir.");
+        }
     }
 }
